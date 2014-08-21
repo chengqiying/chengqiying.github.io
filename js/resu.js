9b8a6c3a5fd5b17422cf461def1a08dd944b06fe
chengqiying <chengqiying@gmail.com>
2014-08-21 23:03:37 +0800

    resu

--- js/resu.js
+++ js/resu.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
-	$('#cv-page').hide(),
 	$('#cv').click(function(){
-		$('#cv-page').FadeIn(500),
+		$('#cv-page').FadeIn(500);
 		$("#all, #border, #footer").hide();
 	});
 });
