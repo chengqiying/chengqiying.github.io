a0a293173801c345ca0ff4ab4f2a4b1e435c37c7
chengqiying <chengqiying@gmail.com>
2014-08-22 15:13:24 +0800

    cvicon

--- js/resu.js
+++ js/resu.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){
+	$('#cv').hover(function(){
+		$('#cv h2').css({color: "#FFFFFF"});
+		$('#cv').addClass("cvcolor1");
+		}, function(){
+			$('#cv h2').css({color: "#CDCDCD"});
+			$('#cv').removeClass("cvcolor1");;
+	});
 	$('#cv').click(function(){
 		$(".footer, #all, #border").hide();
 		$('#cv-page').fadeIn(500);
