f3a68556243f7152fea0f064e060441583d1de2e
chengqiying <qy@QYs-MacBook-Pro.ddns.hightechcampus.nl>
2015-03-19 15:12:54 +0100

    3.19

--- showcase/M22/js/main.js
+++ showcase/M22/js/main.js
@@ -78,16 +78,27 @@ $(document).ready(function(){
       .duration('0.2s')
       .delay('0.2s')
       .end();
+    move('#coffee-back-click')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
     move('#coffee-add')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+     move('#coffee-add-click')
       .set('opacity', 1)
       .duration('0.2s')
       .delay('0.2s')
       .end();
   });
 
-  $('#coffee-back').click(function(){
+  /*to have coffee back*/
+  $('#coffee-back-click').click(function(){
     move('#havecoffee')
-      .to(0, 0)
+      .to(0, 240)
       .set('opacity', 0)
       .duration('0.3s')
       .ease('in')
@@ -110,12 +121,168 @@ $(document).ready(function(){
       .set('opacity', 0)
       .duration('0.2s')
       .end();
+    move('#coffee-back-click')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
     move('#coffee-add')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-add-click')
       .set('opacity', 0)
       .duration('0.2s')
       .end();
   });
 
+  /*input coffee*/
+  $('#coffee-add-click').click(function(){
+
+    move('#inputcoffee')
+      .to(-320, 0)
+      .set('opacity', 1)
+      .duration('0.3s')
+      .ease('in')
+      .end();
+    move('#coffee-cancel')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-cancel-click')
+      .to(-50, 0)
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#havecoffee')
+      .set('opacity', 0)
+      .duration('0.3s')
+      .delay('0.3s')
+      .end();
+    move('#coffee-back')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-back-click')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-add')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-add-click')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+  });
+
+/*cancel coffeeinput*/
+  $('#coffee-cancel-click').click(function(){
+    move('#inputcoffee')
+      .to(0, 0)
+      .set('opacity', 0)
+      .duration('0.3s')
+      .ease('in')
+      .delay('0.3s')
+      .end(); 
+    move('#havecoffee')
+      .set('opacity', 1)
+      .duration('0.3s')
+      .end();
+    move('#coffee-cancel')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-cancel-click')
+      .to(0, 0)
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-back')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-back-click')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-add')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-add-click')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+  });
+
+  /*done coffeeinput*/
+  $('#coffee-done').click(function(){
+    move('#inputcoffee')
+      .to(0, 0)
+      .set('opacity', 0)
+      .duration('0.3s')
+      .ease('in')
+      .delay('0.3s')
+      .end(); 
+    move('#coffee-cancel')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#coffee-cancel-click')
+      .set('opacity', 0)
+      .duration('0.2s')
+      .end();
+    move('#havecoffee')
+      .to(-320, 240)
+      .set('opacity', 1)
+      .set('height', 498)
+      .duration('0.3s')
+      .end();
+    move('#coffee-back')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-back-click')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-add')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#coffee-add-click')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .delay('0.2s')
+      .end();
+    move('#luis')
+      .set('opacity', 1)
+      .set('margin-top', 0)
+      .duration('0.2s')
+      .end();
+    move('#luis-space')
+      .set('opacity', 1)
+      .duration('0.2s')
+      .end();
+  });
+});
+
+
+$('#coffee-done').mousedown(function(){
+    move('#coffee-done')
+      .scale('0.85')
+      .duration('0.5s')
+      .end();
 });
        
 
