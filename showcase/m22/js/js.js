2d6c83f9554676b72a4f04e3e609433e85e78905
chengqiying <qy@QYs-MacBook-Pro.local>
2015-06-18 20:44:25 +0200

    6.18

--- showcase/m22/js/js.js
+++ showcase/m22/js/js.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$('#prototype').hover(function(){
-		$("#prototype").css("background-color", "#BDE9F4");
+		$("#prototype").css("background-color", "#94ebf7");
 		$("#prototype p").css("color", "#ffffff");
 		$("#prototype p").css("text-decoration", "none");
 		}, 
@@ -10,7 +10,7 @@ $(document).ready(function(){
 	});
 
 	$('#feedback').hover(function(){
-		$("#feedback").css("background-color", "#BDE9F4");
+		$("#feedback").css("background-color", "#94ebf7");
 		$("#feedback p").css("color", "#ffffff");
 		$("#feedback p").css("text-decoration", "none");
 		}, 
@@ -20,12 +20,86 @@ $(document).ready(function(){
 	});
 
 	$('#report').hover(function(){
-		$("#report").css("background-color", "#BDE9F4");
+		$("#report").css("background-color", "#94ebf7");
 		$("#report p").css("color", "#ffffff");
 		$("#report p").css("text-decoration", "none");
 		}, 
 		function() {
 			$("#report").css("background-color", "#ffffff");
 			$("#report p").css("color", "#4f4f4f");
+	});
+
+	$('#yelp-seemore').hover(function(){
+		$("#yelp-seemore").css("background-color", "#94ebf7");
+		$("#yelp-seemore p").css("color", "#ffffff");
+		$("#yelp-seemore p").css("text-decoration", "none");
+		}, 
+		function() {
+			$("#yelp-seemore").css("background-color", "#ffffff");
+			$("#yelp-seemore p").css("color", "#4f4f4f");
+	});
+
+	$('#ibm-seemore').hover(function(){
+		$("#ibm-seemore").css("background-color", "#94ebf7");
+		$("#ibm-seemore p").css("color", "#ffffff");
+		$("#ibm-seemore p").css("text-decoration", "none");
+		}, 
+		function() {
+			$("#ibm-seemore").css("background-color", "#ffffff");
+			$("#ibm-seemore p").css("color", "#4f4f4f");
+	});
+
+	$('#insole-seemore').hover(function(){
+		$("#insole-seemore").css("background-color", "#94ebf7");
+		$("#insole-seemore p").css("color", "#ffffff");
+		$("#insole-seemore p").css("text-decoration", "none");
+		}, 
+		function() {
+			$("#insole-seemore").css("background-color", "#ffffff");
+			$("#insole-seemore p").css("color", "#4f4f4f");
+	});
+
+	$('#email').hover(function(){
+		$("#email").css("background-color", "#ffffff");
+		$("#email p").css("color", "#94ebf7");
+		$("#email-icon").css("color", "#94ebf7");
+		}, 
+		function() {
+			$("#email").css("background-color", "#fcfcfc");
+			$("#email p").css("color", "#b7b7b7");
+			$("#email-icon").css("color", "#b7b7b7");
+	});
+
+	$('#linkedin').hover(function(){
+		$("#linkedin").css("background-color", "#ffffff");
+		$("#linkedin p").css("color", "#94ebf7");
+		$("#linkedin-icon").css("color", "#94ebf7");
+		}, 
+		function() {
+			$("#linkedin").css("background-color", "#fcfcfc");
+			$("#linkedin p").css("color", "#b7b7b7");
+			$("#linkedin-icon").css("color", "#b7b7b7");
+	});
+
+	$('#portfolio').hover(function(){
+		$("#portfolio").css("background-color", "#ffffff");
+		$("#portfolio p").css("color", "#94ebf7");
+		$("#portfolio-icon").css("color", "#94ebf7");
+		}, 
+		function() {
+			$("#portfolio").css("background-color", "#fcfcfc");
+			$("#portfolio p").css("color", "#b7b7b7");
+			$("#portfolio-icon").css("color", "#b7b7b7");
+	});
+
+	$('#facebook').hover(function(){
+		$("#facebook").css("background-color", "#ffffff");
+		$("#facebook p").css("color", "#94ebf7");
+		$("#facebook-icon").css("color", "#94ebf7");
+		}, 
+		function() {
+			$("#facebook").css("background-color", "#fcfcfc");
+			$("#facebook p").css("color", "#b7b7b7");
+			$("#facebook-icon").css("color", "#b7b7b7");
 	});
 })
